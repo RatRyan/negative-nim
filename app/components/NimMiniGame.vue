60bd2e0bd8f81c6a543d443ac5fcc7e3f1106472
RatRyan <ryantratajczak@gmail.com>
2024-01-28 16:58:26 -0700

    converted vue 2 api to composition api

--- app/components/NimMiniGame.vue
+++ app/components/NimMiniGame.vue
@@ -1,102 +1,114 @@
 <template>
-    <div id="app">
-      <h1>Nim Game</h1>
-      <div class="heap">
-        <div v-for="count in objects" :key="count" class="stone">
-          🧱
-        </div>
-      </div>
-      <div v-if="!winner" class="controls">
-        <label>Objects to Remove:</label>
-        <select v-model="objectsToRemove" :disabled="currentPlayer === 2">
-          <option v-for="num in [1, 2, 3]" :key="num" :value="num">{{ num }}</option>
-        </select>
-        <button @click="playerRemoveObjects" :disabled="currentPlayer === 2 || objectsToRemove > objects">
-          Remove Objects
-        </button>
-      </div>
-      <div v-if="winner" class="winner">
-        <p>{{ winner }}</p>
-        <button @click="resetGame">Play Again</button>
+  <div id="app">
+    <h1>Nim Game</h1>
+    <div class="heap">
+      <div v-for="count in gameState.objects" :key="count" class="stone">
+        🧱
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        objects: 12,
-        currentPlayer: 1,
-        winner: null,
-        objectsToRemove: 1,
-        playerCanInteract: true,
-      };
-    },
-    methods: {
-      playerRemoveObjects() {
-        if (this.currentPlayer === 1 && !this.winner && this.playerCanInteract) {
-          const objectsToRemove = parseInt(this.objectsToRemove, 10);
-          if (objectsToRemove > 0 && objectsToRemove <= 3 && objectsToRemove <= this.objects) {
-            this.objects -= objectsToRemove;
-            if (this.checkWin()) {
-              this.winner = "Player wins!";
-            } else {
-              this.currentPlayer = 2;
-              this.makeNPCMove();
-            }
-          }
-        }
-      },
-      makeNPCMove() {
-        if (!this.checkWin()) {
-          const npcObjectsToRemove = 4 - this.objectsToRemove;
-          this.objects -= npcObjectsToRemove;
-          if (this.checkWin()) {
-            this.winner = "NPC wins!";
-          } else {
-            this.currentPlayer = 1; // Switch back to the player
-          }
-        }
-      },
-      checkWin() {
-        return this.objects === 0;
-      },
-      resetGame() {
-        this.objects = 12;
-        this.currentPlayer = 1;
-        this.winner = null;
-        this.objectsToRemove = 1;
-        this.playerCanInteract = true;
-      },
-    },
-  };
-  </script>
-  
-  <style>
-  #app {
-    text-align: center;
-    margin-top: 60px;
+    <div v-if="!gameState.winner" class="controls">
+      <label>Objects to Remove:</label>
+      <select
+        v-model="gameState.objectsToRemove"
+        :disabled="gameState.currentPlayer === 2"
+      >
+        <option v-for="num in [1, 2, 3]" :key="num" :value="num">
+          {{ num }}
+        </option>
+      </select>
+      <button
+        @click="playerRemoveObjects"
+        :disabled="
+          gameState.currentPlayer === 2 ||
+          gameState.objectsToRemove > gameState.objects
+        "
+      >
+        Remove Objects
+      </button>
+    </div>
+    <div v-if="gameState.winner" class="winner">
+      <p>{{ gameState.winner }}</p>
+      <button @click="resetGame">Play Again</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const gameState = reactive({
+  objects: Number,
+  currentPlayer: Number,
+  winner: Boolean,
+  objectsToRemove: Number,
+  playerCanInteract: Boolean,
+});
+
+function playerRemoveObjects() {
+  if (
+    gameState.currentPlayer === 1 &&
+    !gameState.winner &&
+    gameState.playerCanInteract
+  ) {
+    const objectsToRemove = parseInt(gameState.objectsToRemove, 10);
+    if (
+      objectsToRemove > 0 &&
+      objectsToRemove <= 3 &&
+      objectsToRemove <= gameState.objects
+    ) {
+      gameState.objects -= objectsToRemove;
+      if (gameState.checkWin()) {
+        gameState.winner = 'Player wins!';
+      } else {
+        gameState.currentPlayer = 2;
+        gameState.makeNPCMove();
+      }
+    }
   }
-  
-  .heap {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 10px;
+}
+function makeNPCMove() {
+  if (!gameState.checkWin()) {
+    const npcObjectsToRemove = 4 - gameState.objectsToRemove;
+    gameState.objects -= npcObjectsToRemove;
+    if (gameState.checkWin()) {
+      gameState.winner = 'NPC wins!';
+    } else {
+      gameState.currentPlayer = 1; // Switch back to the player
+    }
   }
-  
-  .stone {
-    font-size: 24px;
-    margin: 0 5px;
-  }
-  
-  .controls {
-    margin-top: 20px;
-  }
-  
-  .winner {
-    margin-top: 20px;
-    font-size: 18px;
-  }
-  </style>
-  
+}
+function checkWin() {
+  return gameState.objects === 0;
+}
+function resetGame() {
+  gameState.objects = 12;
+  gameState.currentPlayer = 1;
+  gameState.winner = null;
+  gameState.objectsToRemove = 1;
+  gameState.playerCanInteract = true;
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+
+.heap {
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.stone {
+  font-size: 24px;
+  margin: 0 5px;
+}
+
+.controls {
+  margin-top: 20px;
+}
+
+.winner {
+  margin-top: 20px;
+  font-size: 18px;
+}
+</style>
