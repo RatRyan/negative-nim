06a2368b8012f9b0d383c821a8399bcd5910e722
EthanMorin <EthanMorin2004@gmail.com>
2024-01-30 16:41:34 -0700

    Update default.vue
    added game reset

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,85 +1,89 @@
+<script setup lang="ts">
+const { gotoCharacterCreator } = useGameState();
+</script>
+
 <template>
-  <div class="window">
-    <div class="window-header">
-      <div class="window-nav">
-        <div class="title">
-          <BootstrapIcon name="terminal-fill"></BootstrapIcon>
-          <p><b>-Nim (because its like nim minus the nim)</b></p>
-        </div>
-        <div>
-          <button><BootstrapIcon name="dash-lg"></BootstrapIcon></button>
-          <button>
-            <BootstrapIcon name="window-fullscreen"></BootstrapIcon>
-          </button>
-          <button><BootstrapIcon name="x-lg"></BootstrapIcon></button>
-        </div>
-      </div>
-    </div>
-    <div class="content-wrapper">
-      <slot />
-    </div>
-  </div>
+	<div class="window">
+		<div class="window-header">
+			<div class="window-nav">
+				<div class="title">
+					<BootstrapIcon name="terminal-fill"></BootstrapIcon>
+					<p><b>-Nim (because its like nim minus the nim)</b></p>
+				</div>
+				<div>
+					<button><BootstrapIcon name="dash-lg"></BootstrapIcon></button>
+					<button>
+						<BootstrapIcon name="window-fullscreen"></BootstrapIcon>
+					</button>
+					<button><BootstrapIcon name="x-lg" @click="gotoCharacterCreator"></BootstrapIcon></button>
+				</div>
+			</div>
+		</div>
+		<div class="content-wrapper">
+			<slot />
+		</div>
+	</div>
 </template>
 
 <style>
 .window {
-  box-shadow: 3px 3px 16px 12px #212121;
-  background-color: #bec7c9;
-  width: 63vw;
-  height: 60vh;
-  min-width: 1280px;
-  min-height: 720px;
-  padding: 5px;
-  border: outset .2rem white;
-  border-radius: 5px;
-  display: flex;
-  flex-direction: column;
-  position: relative;
+	box-shadow: 3px 3px 16px 12px #212121;
+	background-color: #bec7c9;
+	width: 63vw;
+	height: 60vh;
+	min-width: 1280px;
+	min-height: 720px;
+	padding: 5px;
+	border: outset 0.2rem white;
+	border-radius: 5px;
+	display: flex;
+	flex-direction: column;
+	position: relative;
 }
 
 .window-header {
-  background-color: #0000a8;
-  height: 7%;
-  border: inset 2px;
-  border-radius: 1px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+	background-color: #0000a8;
+	height: 7%;
+	border: inset 2px;
+	border-radius: 1px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 }
 
 .window-nav {
-  flex: 1;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-right: 7px;
+	flex: 1;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-right: 7px;
 }
 
 .title {
-  display: flex;
-  align-items: center;
-  margin-left: 18px;
+	display: flex;
+	align-items: center;
+	margin-left: 18px;
 }
 
 .window-nav p {
-  margin-left: 10px;
+	margin-left: 10px;
 }
 
 .window-nav button {
-  align-items: center;
-  border: outset 3px white;
-  padding: 0;
-  margin: 3px 3px;
-  font-size: 18px;
-  width: 30px;
-  height: 30px;
+	align-items: center;
+	border: outset 3px white;
+	padding: 0;
+	margin: 3px 3px;
+	font-size: 18px;
+	width: 30px;
+	height: 30px;
 }
 
 .content-wrapper {
-  background-color: #161414;
-  height: calc(100% - 18px);
-  overflow: hidden;
-  margin: 30px 5px 5px 5px;
-  border: inset 3px;
+	background-color: #161414;
+	height: calc(100% - 18px);
+	overflow: hidden;
+	margin: 30px 5px 5px 5px;
+	border: inset 3px;
 }
 </style>
