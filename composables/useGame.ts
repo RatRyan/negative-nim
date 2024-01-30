04968e1e2aa06cdd930dcc1d08f6a44f66cb97f0
EthanMorin <EthanMorin2004@gmail.com>
2024-01-30 10:16:22 -0700

    added updates to useGame

--- composables/useGame.ts
+++ composables/useGame.ts
@@ -1,94 +1,99 @@
 export function useGame() {
-  const areas = [
+	const areas = [
+		{
+			name: "forest",
+			dialogue: `You wake up to find yourself lying on the damp forest floor. The canopy overhead filters the sunlight, casting dappled shadows on the ground. The air is thick with the scent of pine and earth. You have no memory of how you got here or where you are. As you stand up, you notice a narrow path winding its way through the trees. The forest seems both mysterious and inviting. You realize that your only choice is to venture forth and discover what lies ahead.`,
+			actions: ["town"],
+		},
+		{
+			name: "town",
+			dialogue:
+				"finally, after hours of walking you come up upon a small town. Do you want to go to the tavern, or continue through the city?",
+			actions: ["tavern", "city"],
+		},
+		{
+			name: "tavern",
+			dialogue:
+				"You find that there is a group at a table glaring at you and talking between themselves, as that goes on the bartender is always extending a hearty hello towards you as you enter.",
+			actions: ["conflict", "bar"],
+		},
+		{
+			name: "conflict",
+			dialogue:
+				"The group sees you approaching and all get up and start heading towards you. You look back at the door and debate running but its too late...",
+			actions: ["lose"],
+		},
+		{
+			name: "bar",
+			dialogue:
+				"As you go to sit down the bar keeper come to you and offers you a drink if you can win in a game of Nim.",
+			actions: ["nim", "leave"],
+		},
     {
-      name: 'forest',
-      dialogue: `You wake up to find yourself lying on the damp forest floor. The canopy overhead filters the sunlight, casting dappled shadows on the ground. The air is thick with the scent of pine and earth. You have no memory of how you got here or where you are. As you stand up, you notice a narrow path winding its way through the trees. The forest seems both mysterious and inviting. You realize that your only choice is to venture forth and discover what lies ahead.`,
-      actions: ['town'],
+      name: "leave",
+      dialogue: "You leave the bar and continue on your journey through the town.",
+      actions: ["city"],
     },
-    {
-      name: 'town',
-      dialogue:
-        'finally, after hours of walking you come up upon a small town. Do you want to go to the tavern, or continue through the city?',
-      actions: ['tavern', 'city'],
-    },
-    {
-      name: 'tavern',
-      dialogue:
-        'You find that there is a group at a table glaring at you and talking between themselves, as that goes on the bartender is always extending a hearty hello towards you as you enter.',
-      actions: ['conflict', 'bar'],
-    },
-    {
-      name: 'conflict',
-      dialogue:
-        'The group sees you approaching and all get up and start heading towards you. You look back at the door and debate running but its too late...',
-      actions: ['end'],
-    },
-    {
-      name: 'bar',
-      dialogue:
-        'As you go to sit down the bar keeper come to you and offers you a drink you accepte and need to dig in your pockets and get some gold to give to the bar tender.',
-      actions: [''],
-    },
-    {
-      name: 'city',
-      dialogue:
-        'Walking through the town the air is filled with the smell of fresh bread being backed and blue skies, but just as you get towards the outskirts of the town you hear a feint cry for help...',
-      actions: [],
-    },
-    {
-      name: 'ignore',
-      dialogue:
-        'You decide to continue on the path of no resistance and remain blissfully ignorant of the screams and will never findout for you will just keep walking for eternity.That is what you did from birth and will continue to do until death.',
-      actions: [],
-    },
-    {
-      name: 'investigate',
-      dialogue:
-        'Wanting to help to help whoever might be in danger you decide to go and investigate the soundsand see a cottage on fire!',
-      actions: [],
-    },
-    {
-      name: 'fire',
-      dialogue:
-        'As you go to sit down the bar keeper come to you and offers you a drink you accept and need to dig in your pockets and get some gold to give to the bar tender.',
-      actions: [],
-    },
-  ];
+		{
+			name: "city",
+			dialogue:
+				"Walking through the town the air is filled with the smell of fresh bread being backed and blue skies, but just as you get towards the outskirts of the town you hear a feint cry for help...",
+			actions: ["ignore", "investigate"],
+		},
+		{
+			name: "ignore",
+			dialogue:
+				"You decide to continue on the path of no resistance and remain blissfully ignorant of the screams and will never findout for you will just keep walking for eternity.That is what you did from birth and will continue to do until death.",
+			actions: ["win"],
+		},
+		{
+			name: "investigate",
+			dialogue:
+				"Wanting to help to help whoever might be in danger you decide to go and investigate the soundsand see a cottage on fire!",
+			actions: ["fire"],
+		},
+		{
+			name: "fire",
+			dialogue:
+				"As you go to sit down the bar keeper come to you and offers you a drink you accept and need to dig in your pockets and get some gold to give to the bar tender.",
+			actions: ["win, lose"],
+		},
+	];
 
-  type GameComponent = 'CharacterCreator' | 'MainGame' | 'GameOver';
-  type GameState = {
-    component: GameComponent;
-    currentArea: {
-      name: string;
-      dialogue: string;
-      actions: string[];
-    };
-  };
-  const gameState = useState<GameState>('gameState', () => ({
-    component: 'MainGame',
-    currentArea: {
-      name: '',
-      dialogue: '',
-      actions: [''],
-    },
-  }));
+	type GameComponent = "CharacterCreator" | "MainGame" | "GameOver";
+	type GameState = {
+		component: GameComponent;
+		currentArea: {
+			name: string;
+			dialogue: string;
+			actions: string[];
+		};
+	};
+	const gameState = useState<GameState>("gameState", () => ({
+		component: "MainGame",
+		currentArea: {
+			name: "",
+			dialogue: "",
+			actions: [""],
+		},
+	}));
 
-  function startGame() {
-    gameState.value.component = 'CharacterCreator';
-  }
+	function startGame() {
+		gameState.value.component = "CharacterCreator";
+	}
 
-  function enterMainGame() {
-    gameState.value.component = 'MainGame';
-  }
+	function enterMainGame() {
+		gameState.value.component = "MainGame";
+	}
 
-  function endGame() {
-    gameState.value.component = 'GameOver';
-  }
+	function endGame() {
+		gameState.value.component = "GameOver";
+	}
 
-  return {
-    gameState,
-    startGame,
-    enterMainGame,
-    endGame,
-  };
+	return {
+		gameState,
+		startGame,
+		enterMainGame,
+		endGame,
+	};
 }
